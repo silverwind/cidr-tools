132f9014bd3249bdf4cf71210888e854110291c5
silverwind <me@silverwind.io>
2017-02-17 00:45:03 +0100

    add a few more exclude tests

--- test.js
+++ test.js
@@ -17,10 +17,22 @@ test("exclude v4 nets", async function(t) {
   t.deepEqual(await m.exclude(["1.0.0.0/23"], ["1.0.1.0/24"]), ["1.0.0.0/24"]);
 });
 
+test("exclude v4 nets #2", async function(t) {
+  t.deepEqual(await m.exclude(["1.0.0.0/24"], ["1.0.0.0/16"]), []);
+});
+
 test("exclude v6 nets", async function(t) {
   t.deepEqual(await m.exclude(["::/127"], ["::1/128"]), ["::/128"]);
 });
 
+test("exclude v6 nets #2", async function(t) {
+  t.deepEqual(await m.exclude(["::/120"], ["::1/112"]), []);
+});
+
 test("exclude mixed", async function(t) {
   t.deepEqual(await m.exclude(["::0/127", "1.2.3.0/24"], ["::/128"]), ["1.2.3.0/24", "::1/128"]);
+});
+
+test("exclude mixed #2", async function(t) {
+  t.deepEqual(await m.exclude(["::0/127", "1.2.3.0/24"], ["::/0", "0.0.0.0/0"]), []);
 });
