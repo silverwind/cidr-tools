d51908ab6008d3b52b4ec29b1ac9e8e190d2985f
Paul Joey Clark <joeytwiddle@gmail.com>
2019-04-09 13:50:51 +0800

    Add TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+type IPv4Address = string;
+type IPv4CIDR = string;
+type IPv6Address = string;
+type IPv6CIDR = string;
+
+type Network = IPv4Address | IPv4CIDR | IPv6Address | IPv6CIDR;
+
+type Networks = Network | Network[];
+
+interface CIDRTools {
+  merge(networks: Networks): Network[];
+  exclude(baseNetworks: Networks, excludeNetworks: Networks): Network[];
+  expand(networks: Networks): Network[];
+  overlap(networksA: Networks, networksB: Networks): boolean;
+  normalize(Network): Network;
+}
+
+declare const cidrTools : CIDRTools;
+
+declare module "cidr-tools" {
+  export = cidrTools;
+}
