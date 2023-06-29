6045cdfb3e21ccd018bb9af80bd1d945b27fda94
silverwind <me@silverwind.io>
2023-06-30 01:12:15 +0200

    update deps

--- vitest.config.js
+++ vitest.config.js
@@ -1,17 +1,6 @@
 import {defineConfig} from "vitest/dist/config.js";
+import {backendTest} from "vitest-config-silverwind";
 
 export default defineConfig({
-  test: {
-    environment: "node",
-    testTimeout: 20000,
-    open: false,
-    allowOnly: true,
-    passWithNoTests: true,
-    globals: true,
-    watch: false,
-    outputDiffLines: Infinity,
-    outputDiffMaxLines: Infinity,
-    outputDiffMaxSize: Infinity,
-    outputTruncateLength: Infinity,
-  },
+  test: backendTest,
 });
